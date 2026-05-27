25cd0a6208e343400b9e2179da5884ad8d7e29e4
Arina-loiko <arina.loikoloiko@yandex.ru>
2026-05-27 22:00:00 +0300

    Configure Program.cs: CORS, SQLite, JSON cycle handling

---
+++ Backend/LabTracker/Program.cs
@@ -0,0 +1,30 @@
+using LabTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=labtracker.db"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+app.UseCors();
+app.MapControllers();
+app.Run();
